d76b441cbef303e44dc1e32e72df61808571a6ae
Gagan6164 <32809202+Gagan6164@users.noreply.github.com>
2019-05-18 16:41:06 +0530

    home page

--- portfolio/src/components/home.js
+++ portfolio/src/components/home.js
@@ -1,15 +1,33 @@
 import React, { Component } from "react";
+import { Element } from "react-scroll";
+import { Link } from "react-scroll";
 import "./home.scss";
-
-class Home extends Component {
+export default class Home extends Component {
   render() {
     return (
-      <div className="home">
+      <Element name="home" className="home">
         <div className="overlay">
           <span />
         </div>
-      </div>
+        <div>
+          <div className="home_name">I am Rohit Jha</div>
+          <div className="home_det">
+            Software Developer | Web Developer | DevOps
+          </div>
+          <div className="home_abt">
+            <Link
+              activeClass="nav_array_active"
+              to="about"
+              spy={true}
+              smooth={true}
+              duration={500}
+              className="nav_items"
+            >
+              About Me
+            </Link>
+          </div>
+        </div>
+      </Element>
     );
   }
 }
-export default Home;
